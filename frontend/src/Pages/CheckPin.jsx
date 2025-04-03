ac8f6fcfbf82602a66d7aa69a0106401986344da
Hritik <hritik.dev.04@gmail.com>
2025-04-03 10:00:00 +0530

    Your commit message

--- frontend/src/Pages/CheckPin.jsx
+++ frontend/src/Pages/CheckPin.jsx
@@ -31,6 +31,9 @@ const [password, setPassword] = useState("");
       }
 
       // Backspace
+      if (e.key === "Backspace") {
+        setPassword((prev) => prev.slice(0, -1)); 
+      }
 
       // Enter key
       if (e.key === "Enter") {
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -7,5 +7,5 @@ const HomePage = () => {
     </>
   )
 }
-
+  
 export default HomePage
