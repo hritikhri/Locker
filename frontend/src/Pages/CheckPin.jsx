441d6f0448677ef949e8adaccf4a6bfda7779eb6
Hritik <hritik.dev.04@gmail.com>
2026-03-27 08:02:26 +0530

    frontend files changed

--- frontend/src/Pages/CheckPin.jsx
+++ frontend/src/Pages/CheckPin.jsx
@@ -4,7 +4,7 @@ export const CheckPin = () => {
   return (
     <>
     <div className="container">
-      <div className="pin"></div>
+      <div className="pin">boxes</div>
       <div className="keyboard"></div>
     </div>
     </>
